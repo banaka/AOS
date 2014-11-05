bc18ae88fe94fe3bd023387fcb2091a36cbf1ddc
banaka <banaka@github.com>
2014-11-05 07:56:01 -0800

    Fixed accept

--- lab4/paxos_exec.cpp
+++ lab4/paxos_exec.cpp
@@ -10,20 +10,21 @@ bool my_trim_func(const std::unique_ptr<Paxlog::tup>& myTup){
 }
 
 void paxserver::execute_arg(const struct execute_arg& ex_arg) {
-	LOG(l::DEBUG, id_str() << " Execute Argument" << ex_arg << " state:"<< paxlog.latest_exec() );
-	//Another way to check Check if the request is already been taken care of
-	/*for (auto it = exec_rid_cache.begin(); it != exec_rid_cache.end(); ++it ){
-		std::cout << (*it).first << (*it).second<<std::endl;
-		if(((*it).first == ex_arg.nid) && ((*it).second == ex_arg.rid)){
-			std::cout << "The request has already been processed" <<std::endl;
+	LOG(l::DEBUG, id_str() << " execute_arg:" << ex_arg << " state:"<< paxlog.latest_exec() <<"\n" );
+	//Way to check Check if the request is already been taken care of	
+	for (auto it = exec_rid_cache.begin(); it != exec_rid_cache.end(); ++it ){
+	        //std::cout << (*it).first << (*it).second<<std::endl;
+			if(((*it).first == ex_arg.nid) && ((*it).second == ex_arg.rid)){
+			LOG(l::DEBUG, id_str() << "The request has already been processed\n");
+			net->drop(this, ex_arg, " The request has already been processsed.");
 			return;
 		}
-	}*/
+	}
 
-	if(primary()){
+	if(primary()){		
 		if(paxlog.find_rid(ex_arg.nid, ex_arg.rid)){
-			LOG(l::DEBUG, id_str() << " find_rid Failed, Probably the request has already been processsed." );
-			net->drop(this, ex_arg, "The request has already been processed");
+			LOG(l::DEBUG, id_str() << " find_rid Pass, The request is being  processsed." <<"\n" );
+			net->drop(this, ex_arg, " The request is being  processsed.");
 			return;
 		}
 
@@ -44,7 +45,7 @@ void paxserver::execute_arg(const struct execute_arg& ex_arg) {
 			auto new_rep_arg = std::make_unique<struct replicate_arg>(new_vs, ex_arg, paxlog.latest_exec());
 			net->send(this, serv, std::move(new_rep_arg));
 		}
-		LOG(l::DEBUG, id_str() << " Logged and sent for Vote for Request:" << ex_arg.request );
+		LOG(l::DEBUG, id_str() << " Logged and sent for Vote for Request:" << ex_arg.request <<"\n" );
 	}else{
 		//Send execute_fail to the client so that it can send request to the correct Primary
 		auto new_execute_fail = std::make_unique<struct execute_fail>(vc_state.view.vid, vc_state.view.primary, ex_arg.rid);
@@ -56,7 +57,7 @@ void paxserver::execute_arg(const struct execute_arg& ex_arg) {
 void paxserver::replicate_arg(const struct replicate_arg& repl_arg) {
 	//1. Try and execute the operations untill the committed in the message and update lastest_seen 
 	//2. Reply with the Viewstamp
-	LOG(l::DEBUG, id_str() << id_str() << " Replicate Argument" << repl_arg << " State:"<< paxlog.latest_exec() );
+	LOG(l::DEBUG, id_str() << " replicate_arg:" << repl_arg << " latest_exec:"<< paxlog.latest_exec() <<"\n" );
 	if(!primary()) {
 		//Execute all the Operatins the Replica Can
 	    for(auto it = paxlog.begin(); it != paxlog.end(); ++it) {
@@ -70,7 +71,7 @@ void paxserver::replicate_arg(const struct replicate_arg& repl_arg) {
 				it = paxlog.begin();
 				if(paxlog.latest_accept() <= repl_arg.committed)
 					paxlog.set_latest_accept(repl_arg.committed);
-				LOG(l::DEBUG, id_str() << " Executed:" << paxlog.latest_exec() << " result:" << result);
+				LOG(l::DEBUG, id_str() << " Executed:" << paxlog.latest_exec() << " result:" << result  <<"\n");
 			}
 		}
 		paxlog.trim_front(my_trim_func);
@@ -82,7 +83,7 @@ void paxserver::replicate_arg(const struct replicate_arg& repl_arg) {
 		//First log then send ACK 
 		auto new_rep_res = std::make_unique<struct replicate_res>(repl_arg.vs);
 		net->send(this, vc_state.view.primary, std::move(new_rep_res)); 
-		LOG(l::DEBUG, id_str() << " Logged and Ack the Request from Primary" );
+		LOG(l::DEBUG, id_str() << " Logged and Ack the Request from Primary"  <<"\n");
 	}
 }
 
@@ -90,10 +91,10 @@ void paxserver::replicate_arg(const struct replicate_arg& repl_arg) {
 void paxserver::replicate_res(const struct replicate_res& repl_res) {
 	//1. Log the respone only primary should be getting these messages.
 	//2. When the count of the response is more than half the server count Send Accept_arg message to all
-	LOG(l::DEBUG, id_str() << " Replicate response" << repl_res << " State:" << paxlog.latest_exec());
+	LOG(l::DEBUG, id_str() << " replicate_res:" << repl_res << " latest_exec:" << paxlog.latest_exec() <<"\n");
 	if(primary()) {
 		if(!paxlog.incr_resp(repl_res.vs)){
-			LOG(l::DEBUG, id_str() << " Got request for already executed request and then truncated from log" );
+			LOG(l::DEBUG, id_str() << " Got request for already executed request and then truncated from log"  <<"\n" );
 			return;
 		}
 								      
@@ -102,25 +103,27 @@ void paxserver::replicate_res(const struct replicate_res& repl_res) {
 				paxlog.execute(*it);
 				std::string result = paxop_on_paxobj(*it);
 				vc_state.latest_seen.ts += 1;
-				LOG(l::DEBUG, id_str() << " Executed:" << paxlog.latest_exec() << " result:" << result);
+				LOG(l::DEBUG, id_str() << " Executed:" << paxlog.latest_exec() << " result:" << result  <<"\n");
 				//Respond to the client
 				auto new_exe_succ = std::make_unique<struct execute_success>(result, (*it)->rid);
 				net->send(this, (*it)->src, std::move(new_exe_succ));          
-				//send Accept arg msgs to The rest of the servers 
-				std::set<node_id_t> servers = get_other_servers(vc_state.view);
-				for(const auto& server:servers) {		
-					auto new_acc_arg = std::make_unique<struct accept_arg>(repl_res.vs);
-					net->send(this, server, std::move(new_acc_arg)); 	
-				}
 			}
 		}
 		paxlog.trim_front(my_trim_func);
+		if(paxlog.empty()){//(paxlog.begin() == paxlog.end()){
+			//send Accept messages to rest of the servers
+			std::set<node_id_t> servers = get_other_servers(vc_state.view);
+			for(const auto& server:servers) {		
+				auto new_acc_arg = std::make_unique<struct accept_arg>(repl_res.vs);
+				net->send(this, server, std::move(new_acc_arg)); 	
+			}
+		}
 	}
    //MASSERT(0, "replicate_res not implemented\n");
 }
 
 void paxserver::accept_arg(const struct accept_arg& acc_arg) {
-	LOG(l::DEBUG, id_str() <<" Accept response" << acc_arg << " latest_exec:"<<  paxlog.latest_exec() );	
+	LOG(l::DEBUG, id_str() <<" accept_arg:" << acc_arg << " latest_exec:"<<  paxlog.latest_exec()  <<"\n" );	
 
 	for(auto it = paxlog.begin(); it != paxlog.end(); ++it) {
 		//if((paxlog.latest_exec() <= acc_arg.committed) && paxlog.next_to_exec(it)){
@@ -128,8 +131,8 @@ void paxserver::accept_arg(const struct accept_arg& acc_arg) {
 			paxlog.execute(*it);
 			std::string result = paxop_on_paxobj(*it);
 			vc_state.latest_seen.ts += 1;
-			LOG(l::DEBUG, id_str() << " Executed:" << paxlog.latest_exec() << " result:" << result);
-			//it = paxlog.begin();
+			LOG(l::DEBUG, id_str() << " Executed:" << paxlog.latest_exec() << " result:" << result  <<"\n");
+			it = paxlog.begin();
 		}
 	}
 	paxlog.trim_front(my_trim_func);
